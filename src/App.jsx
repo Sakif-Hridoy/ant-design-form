cc16053f78767c1b046626e32c0b02e8b8b7612e
Sakif Hridoy <sakifworkspace@gmail.com>
2024-04-23 13:27:27 +0600

    a simple form with ant design to show user input

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AntForm from './components/AntForm'
+
+function App() {
+
+  return (
+    <>
+    <AntForm></AntForm>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AntForm.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import "./AntForm.css";
+import { Button, Checkbox, Form, Input } from "antd";
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+const AntForm = () => {
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
+
+  return (
+    <div>
+      <div className="form-style">
+        <Form
+          name="name"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          style={{
+            maxWidth: 600,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input type="text" onBlur={handleChange} />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password onBlur={handleChange} />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+            <h2>Please Check Browser Console for user input</h2>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default AntForm;
